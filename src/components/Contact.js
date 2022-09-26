079dcc1337cfd6b0dbeb32530a4d62bf7de9eedb
Ferdi Hasan <ferdihasanpwd.com>
2022-09-26 14:01:15 +0700

    Task update error 2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,34 +8,40 @@ import { ListItem, List, ListItemAvatar, Typography, Divider, Box, Avatar } from
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
+
+const contactsList = contactList => {
+  return contactList.map(data => {
+      return (<>
+        <ListItem key={data.email} button alignItems="flex-start" sx={{display: 'flex', flexDirection: 'row', marginRadius: '5%', width: '450px'}}>
+          <Box paddingRight="1em">
+            <ListItemAvatar>
+              <Avatar
+                sx={{width: 60, height: 60}}
+                src={data.photo}
+                alt={data.name}
+              />
+            </ListItemAvatar>
+          </Box>
+          <Box sx={{textAlign: 'start', display: 'flex', flexDirection: 'column'}}>
+            <Typography>{data.name}</Typography>
+            <Typography>{data.phone}</Typography>
+            <Typography>{data.email}</Typography>
+          </Box>
+        </ListItem>
+        <Divider/>
+        </>
+        )
+      })
+}
+// 
+
 const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
 
 
     return (<>
         <List sx={{display: 'flex', flexDirection: 'column',maxWidth: '500px', bgcolor: '#DBF6F0', margin: 'auto', borderRadius: '1%'}}> 
-          {data.map((data) => {
-        return (<>
-          <ListItem key={data.phone.toString()} button alignItems="flex-start" sx={{display: 'flex', flexDirection: 'row', marginRadius: '5%', width: '450px'}}>
-            <Box paddingRight="1em">
-              <ListItemAvatar>
-                <Avatar
-                  sx={{width: 60, height: 60}}
-                  src={data.photo}
-                  alt={data.name}
-                />
-              </ListItemAvatar>
-            </Box>
-            <Box sx={{textAlign: 'start', display: 'flex', flexDirection: 'column'}}>
-              <Typography>{data.name}</Typography>
-              <Typography>{data.phone}</Typography>
-              <Typography>{data.email}</Typography>
-            </Box>
-          </ListItem>
-          <Divider/>
-          </>
-          )
-        })}
+          {contactsList(data)}
         </List>
       </>);
 };
